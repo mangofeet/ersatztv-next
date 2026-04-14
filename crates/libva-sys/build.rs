6969180e84e14718f609929b39738f985087deb6
Jason Dove <1695733+jasongdove@users.noreply.github.com>
2026-04-13 23:47:20 -0500

    build: try to fix windows again

--- crates/libva-sys/build.rs
+++ crates/libva-sys/build.rs
@@ -1,7 +1,8 @@
 fn main() {
     let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
-    if target_os == "linux" && pkg_config::probe_library("libva").is_err()
-        || pkg_config::probe_library("libva-drm").is_err()
+    if target_os == "linux"
+        && (pkg_config::probe_library("libva").is_err()
+            || pkg_config::probe_library("libva-drm").is_err())
     {
         println!("cargo:rustc-link-lib=va");
         println!("cargo:rustc-link-lib=va-drm");
